d49137583bb7800efb51ba77ab266941f4545a30
sarathchand <sarathchand@missionrnd.com>
2016-03-30 23:51:22 +0530

    Submit Commit

--- src/merge2LinkedLists.cpp
+++ src/merge2LinkedLists.cpp
@@ -19,5 +19,30 @@ struct node {
 };
 
 struct node * merge2LinkedLists(struct node *head1, struct node *head2) {
-	return NULL;
+	if ((head1 == NULL) && (head2 == NULL))
+	{
+		return NULL;
+	}
+	else
+	{
+		if ((head1 == NULL) && (head2 != NULL))
+		{
+			return head2;
+		}
+		if ((head1 != NULL) && (head2 == NULL))
+		{
+			return head1;
+		}
+		if (head1->num < head2->num)
+		{
+			head1->next = merge2LinkedLists(head1->next, head2);
+			return head1;
+		}
+		else
+		{
+			head2->next = merge2LinkedLists(head1, head2->next);
+			return head2;
+		}
+	}
+	
 }
